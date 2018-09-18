b5eb415f09bf4c8e0673a4c894b4a94af31be229
Ashutosh Kumar Singh <ashutosh_ks@yahoo.com>
2018-09-19 03:19:57 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <html>
   <head>
     <title>
-      educom - a web app for software professionals
+      educom|Home|PHP
     </title>
   </head>
-  <body bgcolor="7b68ee">
+  <body bgcolor="lavender">
     <h1>educom welcomes you all</h1>
     <h2>This is an educative site</h2>
     <h3>Web development using PHP as back-end scripting language</h3>
     <?PHP
-      echo("Hello world! Ashutosh")."<br>";
+      echo("Hello world! Ashutosh here")."<br>";
       echo("This web app is built using PHP");
     ?>
   </body>
