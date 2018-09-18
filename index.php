47725c18d84fa4e8a29229a1b2a9ee48bca2a668
Ashutosh Kumar Singh <ashutosh_ks@yahoo.com>
2018-09-19 03:50:50 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,8 +7,14 @@
   <body bgcolor="lavender">
     <center>
       <?php
-        print_r(getdate());
-      ?> 
+        // Print the array from getdate()
+        //print_r(getdate());
+        echo "<br><br>";
+
+    // Return date/time info of a timestamp; then format the output
+    $mydate=getdate(date("U"));
+    echo "$mydate[weekday], $mydate[month] $mydate[mday], $mydate[year]";
+    ?>
       <h1>
       <?PHP
       echo("Hello world! Ashutosh here")."<br>";
