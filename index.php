8629006ec1e5ba73b082b5b1a423f9051bcdf7ac
Ashutosh Kumar Singh <ashutosh_ks@yahoo.com>
2018-09-19 03:07:30 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <html>
-<head>
-<title>
-</title>
-</head>
-<body>
-<?PHP
-echo("Hello world! Ashutosh");
-?>
-</body>
+  <head>
+    <title>
+      educom - a web app for software professionals
+    </title>
+  </head>
+  <body bgcolor=>
+    <h1>educom welcomes you all</h1>
+    <?PHP
+      echo("Hello world! Ashutosh");
+    ?>
+  </body>
 </html>
