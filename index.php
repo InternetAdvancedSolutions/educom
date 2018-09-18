e53cfa1a80264d3e4e8772eb91e2183c65c65084
Ashutosh Kumar Singh <ashutosh_ks@yahoo.com>
2018-09-19 03:39:56 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,11 +5,16 @@
     </title>
   </head>
   <body bgcolor="lavender">
-    <center><h1><?PHP
+    <center>
+      <h1>
+      <?PHP
       echo("Hello world! Ashutosh here")."<br>";
-      ?></h1></center>
-    <center><h2><? php
+      ?>
+      </h1>
+      <h2>
+      <?php
       echo("This web app is built using PHP");
-      ?></h2></center>
+      ?></h2>
+    </center>
   </body>
 </html>
