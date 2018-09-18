01538f2dad856baff0a35b3abef947c8ed8875cf
Ashutosh Kumar Singh <ashutosh_ks@yahoo.com>
2018-09-19 03:23:23 +0530

    home page

--- index.php
+++ index.php
@@ -1,13 +1,10 @@
 <html>
   <head>
     <title>
-      educom|Home|PHP
+      educom | Home | PHP
     </title>
   </head>
   <body bgcolor="lavender">
-    <h1>educom welcomes you all</h1>
-    <h2>This is an educative site</h2>
-    <h3>Web development using PHP as back-end scripting language</h3>
     <?PHP
       echo("Hello world! Ashutosh here")."<br>";
       echo("This web app is built using PHP");
