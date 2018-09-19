f62d7c72e7bb168236fc6f2ea9767c3f83fc25ea
Ashutosh Kumar Singh <ashutosh_ks@yahoo.com>
2018-09-19 13:40:26 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   </head>
   <body bgcolor="lavender">
     <center>
-      <h1>Educom</h1><h6>ask, answer , learn</h6>
+      <h1>EDUCOM</h1><h4>ask, answer , learn</h4>
       <?php
     // Return date/time info of a timestamp; then format the output
     $mydate=getdate(date("U"));
@@ -19,9 +19,6 @@
       ?>
       </h2>
       <h2>
-      <?php
-      echo("This web app is written in PHP");
-      ?></h2>
       <table border=1 bgcolor=papayawhip align="center" cellpadding=10 cellspacing=10 width=75% >
       <tr >
         <td>C</td><td>Data Structures in C</td><td>Java</td><td>PHP</td><td>Linux</td><td>Video Tutorials</td>
@@ -30,7 +27,11 @@
         <td><a href="https://deepc.quora.com/">Deep C</a></td><td><a href="http://codelybrary.blogspot.com/">Codelybrary</a></td><td><a href="https://rejavanate.quora.com/">ReJavanate</a></td><td><a href="https://yashphp.quora.com/">Yes PHP!</a></td><td><a href="https://codelybrary.blogspot.com/2018/02/know-me-better.html">Administrator</a></td><td><a href="https://www.youtube.com/watch?v=F43ty4GtrgY&list=UUG-97CfPdyMsjayb-PrBbIg&index=7">My Videos</a></td>
       </tr>
       </table>
-      <h2> Hope you enjoyed learning with us, keep visiting for more exiting stuff on computer science, IT and web development</h2>
+      <h23 Hope you enjoyed learning with us, keep visiting for more exiting stuff on computer science, IT and web development</h3>
+        <h4>
+      <?php
+      echo("This web app is written in PHP");
+      ?></h4>
     </center>
   </body>
 </html>
