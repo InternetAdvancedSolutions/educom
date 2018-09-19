f0473619ad3e60d4f0e8b37d9e5d36a134e0b34f
Ashutosh Kumar Singh <ashutosh_ks@yahoo.com>
2018-09-19 11:18:59 +0530

    Update index.php

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
       <?php
       echo("This web app is built using PHP");echo "<br>"; echo("This is a PHP file");
       ?></h2>
-      <table border=1 bgcolor=papayawhip>
+      <table border=1 bgcolor=papayawhip align=center cellpadding=10 cellspacing=10 width=75% summary=tutorial links>
       <tr >
         <td>C</td><td>DS</td><td>Java</td><td>PHP</td><td>Linux</td><td>Python</td>
       </tr>
