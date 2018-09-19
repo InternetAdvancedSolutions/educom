75dd38f57695f92a68ee864ed9dbb1a838238662
Ashutosh Kumar Singh <ashutosh_ks@yahoo.com>
2018-09-19 13:42:28 +0530

    Update index.php

--- index.php
+++ index.php
@@ -27,11 +27,11 @@
         <td><a href="https://deepc.quora.com/">Deep C</a></td><td><a href="http://codelybrary.blogspot.com/">Codelybrary</a></td><td><a href="https://rejavanate.quora.com/">ReJavanate</a></td><td><a href="https://yashphp.quora.com/">Yes PHP!</a></td><td><a href="https://codelybrary.blogspot.com/2018/02/know-me-better.html">Administrator</a></td><td><a href="https://www.youtube.com/watch?v=F43ty4GtrgY&list=UUG-97CfPdyMsjayb-PrBbIg&index=7">My Videos</a></td>
       </tr>
       </table>
-      <h23 Hope you enjoyed learning with us, keep visiting for more exiting stuff on computer science, IT and web development</h3>
-        <h4>
+      <h3> Hope you enjoyed learning with us, keep visiting for more exiting stuff on computer science, IT and web development</h3>
+        <h5>
       <?php
       echo("This web app is written in PHP");
-      ?></h4>
+      ?></h5>
     </center>
   </body>
 </html>
