dcfc96f57a62fe4d5868834d23e2476c8a2a295c
Ashutosh Kumar Singh <ashutosh_ks@yahoo.com>
2018-09-19 13:36:42 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   </head>
   <body bgcolor="lavender">
     <center>
-      <h1>Educom Education Aids</h1>ask, answer , learn.
+      <h1>Educom</h1><h6>ask, answer , learn</h6>
       <?php
     // Return date/time info of a timestamp; then format the output
     $mydate=getdate(date("U"));
@@ -14,14 +14,13 @@
     ?>
       <h2>
       <?PHP
-      echo("Hello world! I Ashutosh, co-founder educom, BTech, MCA(IGNOU)")."<br>";
+      echo("Hello world! I Ashutosh, co-founder Educom, BTech, MCA(IGNOU)")."<br>";
         echo("welcomes you all tech geeks/students to learn and share");
       ?>
       </h2>
       <h2>
       <?php
-      echo("This web app is built using PHP");
-      echo("It is a PHP file");
+      echo("This web app is written in PHP");
       ?></h2>
       <table border=1 bgcolor=papayawhip align="center" cellpadding=10 cellspacing=10 width=75% >
       <tr >
@@ -31,6 +30,7 @@
         <td><a href="https://deepc.quora.com/">Deep C</a></td><td><a href="http://codelybrary.blogspot.com/">Codelybrary</a></td><td><a href="https://rejavanate.quora.com/">ReJavanate</a></td><td><a href="https://yashphp.quora.com/">Yes PHP!</a></td><td><a href="https://codelybrary.blogspot.com/2018/02/know-me-better.html">Administrator</a></td><td><a href="https://www.youtube.com/watch?v=F43ty4GtrgY&list=UUG-97CfPdyMsjayb-PrBbIg&index=7">My Videos</a></td>
       </tr>
       </table>
+      <h2> Hope you enjoyed learning with us, keep visiting for more exiting stuff on computer science, IT and web development</h2>
     </center>
   </body>
 </html>
