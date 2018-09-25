6b4f48a998c6c7294065876fda16cda718c00f89
Ashutosh Kumar Singh <ashutosh_ks@yahoo.com>
2018-09-25 12:43:47 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,26 @@
   <body bgcolor="lavender">
     <center>
       <h1>EDUCOM</h1><h4>for(i = -&nbsp&#x221e;; i < +&nbsp&#x221e;; i++){ask; answer; learn;}</h4>
+      <?php 
+
+$handle = fopen("counter.txt", "r");
+if(!$handle){
+	
+ echo "could not open the file" ;
+
+}
+else {
+	
+	
+	$counter = (int ) fread($handle,20);
+	fclose ($handle);
+	$counter++;
+	echo" <strong> you are visitor no ". $counter . " </strong> " ;
+$handle = fopen("counter.txt", "w" );
+fwrite($handle,$counter) ;
+fclose ($handle) ;
+	}
+?>
       <hr>
       
       <?php
