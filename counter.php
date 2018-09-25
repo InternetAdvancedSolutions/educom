59111350a426bc5c3d690331339f11d162979948
Ashutosh Kumar Singh <ashutosh_ks@yahoo.com>
2018-09-25 13:34:06 +0530

    Create counter.php

---
+++ counter.php
@@ -0,0 +1,15 @@
+<?php 
+$handle = fopen("counter.txt", "r");
+if(!$handle){	
+ echo "could not open the file" ;
+}
+else {
+	$counter = (int ) fread($handle,20);
+	fclose ($handle);
+	$counter++;
+	echo "You are visitor no ". $counter . "</td></tr></table>" ;
+$handle = fopen("counter.txt", "w" );
+fwrite($handle,$counter) ;
+fclose ($handle) ;
+	}
+  ?>
