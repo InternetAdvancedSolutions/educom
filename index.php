27eead352e90a3f1e8625214167a518af9417f58
Ashutosh Kumar Singh <ashutosh_ks@yahoo.com>
2018-10-02 04:18:55 +0530

    Update index.php

--- index.php
+++ index.php
@@ -55,6 +55,7 @@ window.onload = function() {
       </tr>
       </table>
       Hope you enjoyed learning with us, keep visiting for more exciting stuff on computer science, IT and web development.<br>
+	    Good online resources for programmers:<br>
 	    <ol type="1">
 		    <li>
 	    <a href="https://codegym.cc/quests/tutorial/landing?utm_source=quora&utm_medium=topic_target_desktop">Learn Programming</a>
