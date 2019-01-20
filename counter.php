8d480602e2c44f0e501c824d035f41db6f091021
Ashutosh Kumar Singh <ashutosh_ks@yahoo.com>
2019-01-21 01:35:49 +0530

    Update counter.php

--- counter.php
+++ counter.php
@@ -5,14 +5,22 @@ if(!$handle){
 }
 else 
 {
-	$counter = (int)fgets($handle,1000);
+	//$counter = (int)fgets($handle,1000);
+	// prints a single line at a time until end of file is reached 
+	while (! feof ($handle)) 
+ 	 { 
+		$counter = (int)fgets($handle);
+  	    //echo fgets($my_file); 
+  	} 
 	//$counter = (int ) fread($handle,20);
-	//fclose ($handle);
+	fclose ($handle);
 	$counter++;
-	fwrite($handle,$counter) ;
+	
 	echo "You are visitor no ".$counter. "</td></tr></table>" ;
 //$handle = fopen("counter.txt", "w" );
 //fwrite($handle,$counter) ;
+	$handle = fopen("counter.txt", "a" );
+	fwrite($handle, "\n". $counter);
 fclose ($handle) ;
 }
   ?>
