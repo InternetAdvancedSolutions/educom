d7a3a2348a8f945309bda5fa00135d2a84130f5f
Ashutosh Kumar Singh <ashutosh_ks@yahoo.com>
2019-01-21 01:16:59 +0530

    Update counter.php

--- counter.php
+++ counter.php
@@ -1,15 +1,15 @@
 <?php 
-$handle = fopen("counter.txt", "r");
+$handle = fopen("counter.txt", "rw");
 if(!$handle){	
  echo "could not open the file" ;
 }
 else {
 	$counter = (int)fgets($handle,1000);
 	//$counter = (int ) fread($handle,20);
-	fclose ($handle);
+	//fclose ($handle);
 	$counter++;
 	echo "You are visitor no ".$counter. "</td></tr></table>" ;
-$handle = fopen("counter.txt", "w" );
+//$handle = fopen("counter.txt", "w" );
 fwrite($handle,$counter) ;
 fclose ($handle) ;
 	}
